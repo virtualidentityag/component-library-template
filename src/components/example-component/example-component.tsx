01e30c1b536d4c3d4a8efc80136c5a7ec0de2871
Rebecca Emmanuel <rebeccaemmanuel1995@gmail.com>
2020-07-31 15:27:54 +0200

    refactor: 💡 example component

--- src/components/example-component/example-component.tsx
+++ src/components/example-component/example-component.tsx
@@ -6,13 +6,12 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class ExampleComponent {
-
   render() {
     return (
-      <div><h1>A little component to show you cool stuff</h1>
-      <p>Here we use a Knob to play with this component in the preview.</p>
-      <p>And here we use Faker to generate some content:</p></div>
+      <div>
+        <h1>A little component to show you cool stuff</h1>
+        <p>Here we use a Knob to play with this component in the preview: <slot></slot></p>
+      </div>
     );
   }
-
 }
