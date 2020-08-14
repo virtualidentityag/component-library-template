0288d34cb1285bc124c72e4e325f4abe48387c1f
Rebecca Emmanuel <rebeccaemmanuel1995@gmail.com>
2020-08-14 11:20:13 +0200

    refactor: 💡 delete init.js after initialization

--- .scripts/init.js
+++ .scripts/init.js
@@ -1,35 +1,39 @@
-const replace = require('replace');
-const inquirer = require('inquirer');
+const replace = require("replace");
+const inquirer = require("inquirer");
+const fs = require('fs-extra');
 
 inquirer
   .prompt([
     {
-      type: 'string',
-      name: 'projectName',
+      type: "string",
+      name: "projectName",
       message: "What's the project's name?",
     },
   ])
   .then((answers) => {
     // eslint-disable-next-line no-console
-    console.log('Initializing...');
+    console.log("Initializing...");
     replace({
-      regex: 'component--library',
+      regex: "component--library",
       replacement: answers.projectName,
-      paths: ['./package-lock.json', './package.json', './stencil.config.ts'],
+      paths: ["./package-lock.json", "./package.json", "./stencil.config.ts"],
       recursive: false,
       silent: true,
     });
     replace({
-      regex: 'component--library',
+      regex: "component--library",
       replacement: answers.projectName,
-      paths: ['./.storybook', './loader', './src'],
+      paths: ["./.storybook", "./loader", "./src", "./testing"],
       recursive: true,
       silent: true,
     });
+  })
+  .then(() => {
+    fs.unlink("./.scripts/init.js");
     // eslint-disable-next-line no-console
-    console.log('Initilization done');
+    console.log("Initilization done");
   })
   .catch((err) => {
     // eslint-disable-next-line no-console
-    console.log('Ooops...something went wrong?! '+err);
+    console.log("Ooops...something went wrong?! " + err);
   });
