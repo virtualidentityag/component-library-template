6f65f3fda5889dcf91c308671df700fed310c360
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2020-08-17 13:25:51 +0100

    Feature: Component script (#58)
    * fix: stories file location
    
    * fix: console output
    
    * feat: add css to scss file change
    
    * fix: fix .scripts folder linting
    
    * fix: add .scripts folder to eslint

--- .scripts/init.js
+++ .scripts/init.js
@@ -1,42 +1,42 @@
-const replace = require("replace");
-const inquirer = require("inquirer");
-const fs = require('fs-extra');
+// Disabling these rules due to being a node file
+/* eslint-disable no-console */
 
-inquirer
-  .prompt([
+const { prompt } = require('inquirer');
+const { readFile, writeFile, unlink } = require('fs-extra');
+const replace = require('replace');
+
+(async () => {
+  const { projectName } = await prompt([
     {
-      type: "string",
-      name: "projectName",
+      type: 'string',
+      name: 'projectName',
       message: "What's the project's name?",
     },
-  ])
-  .then((answers) => {
-    // eslint-disable-next-line no-console
-    console.log("Initializing...");
-    replace({
-      regex: "component--library",
-      replacement: answers.projectName,
-      paths: ["./package-lock.json", "./package.json", "./stencil.config.ts"],
-      recursive: false,
-      silent: true,
-    });
-    replace({
-      regex: "component--library",
-      replacement: answers.projectName,
-      paths: ["./.storybook", "./loader", "./src", "./testing"],
-      recursive: true,
-      silent: true,
-    });
-  })
-  .then(() => {
-    const packageJson=JSON.parse(fs.readFileSync("./package.json", "utf-8"));
-    packageJson.scripts.init='echo \"Already initialized!\"';
-    fs.writeFileSync("./package.json", JSON.stringify(packageJson, undefined, 2));
-    fs.unlink("./.scripts/init.js");
-    // eslint-disable-next-line no-console
-    console.log("Initilization done");
-  })
-  .catch((err) => {
-    // eslint-disable-next-line no-console
-    console.log("Ooops...something went wrong?! " + err);
+  ]);
+
+  console.log('Initializing...');
+  replace({
+    regex: 'component--library',
+    replacement: projectName,
+    paths: ['./package-lock.json', './package.json', './stencil.config.ts'],
+    recursive: false,
+    silent: true,
   });
+  replace({
+    regex: 'component--library',
+    replacement: projectName,
+    paths: ['./.storybook', './loader', './src', './testing'],
+    recursive: true,
+    silent: true,
+  });
+
+  // Re-write "init" script
+  const packageJson = JSON.parse(await readFile('./package.json', 'utf-8'));
+  packageJson.scripts.init = 'echo "Already initialized!"';
+  await writeFile('./package.json', JSON.stringify(packageJson, undefined, 2));
+
+  // Remove "init.js" helper
+  await unlink('./.scripts/init.js');
+
+  console.log('Initialization done');
+})();
