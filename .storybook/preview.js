c750087178911cdb782f8e1e1dcd44b8a1a3d90a
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2020-08-21 19:26:45 +0100

    Chore/remove unused flag (#80)
    * chore: remove unused flag
    
    * chore: lint

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { addDecorator } from '@storybook/html';
 import { withTests } from '@storybook/addon-jest';
+// eslint-disable-next-line import/extensions,import/namespace,import/named
 import { defineCustomElements } from '../dist/esm/loader';
 import results from '../.jest-test-results.json';
 
