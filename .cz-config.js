768dccc85ca55fbea104bc5f6dc603877e6a3d5e
Kai Wissler <kai.wissler@Hulk.local>
2020-09-01 11:08:42 +0200

    feat: add emojis to commitizen types

--- .cz-config.js
+++ .cz-config.js
@@ -1,28 +1,28 @@
 module.exports = {
     "types": [{
         "value": "feat",
-        "name": "feat:     A new feature"
+        "name": "🎁 feat:     A new feature"
     }, {
         "value": "fix",
-        "name": "fix:      A bug fix"
+        "name": "👾 fix:      A bug fix"
     }, {
         "value": "docs",
-        "name": "docs:     Documentation only changes"
+        "name": "📖 docs:     Documentation only changes"
     }, {
         "value": "style",
-        "name": "style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)"
+        "name": "🎨 style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)"
     }, {
         "value": "refactor",
-        "name": "refactor: A code change that neither fixes a bug nor adds a feature"
+        "name": "🧹 refactor: A code change that neither fixes a bug nor adds a feature"
     }, {
         "value": "perf",
-        "name": "perf:     A code change that improves performance"
+        "name": "🏎 perf:     A code change that improves performance"
     }, {
         "value": "test",
-        "name": "test:     Adding missing tests"
+        "name": "🔬‍ test:     Adding missing tests"
     }, {
         "value": "chore",
-        "name": "chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation"
+        "name": "🛒 chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation"
     }],
     "scopes": []
 }
