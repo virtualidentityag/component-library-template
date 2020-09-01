bf03f2c80d32c943132c366eb5b33b36db44eea9
Kai Wissler <kai.wissler@Hulk.local>
2020-09-01 11:26:25 +0200

    👾 fix: add emojis to type values as well

--- .cz-config.js
+++ .cz-config.js
@@ -1,27 +1,27 @@
 module.exports = {
     "types": [{
-        "value": "feat",
+        "value": "🎁 feat",
         "name": "🎁 feat:     A new feature"
     }, {
-        "value": "fix",
+        "value": "👾 fix",
         "name": "👾 fix:      A bug fix"
     }, {
-        "value": "docs",
+        "value": "📖 docs",
         "name": "📖 docs:     Documentation only changes"
     }, {
-        "value": "style",
+        "value": "🎨 style",
         "name": "🎨 style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)"
     }, {
-        "value": "refactor",
+        "value": "🧹 refactor",
         "name": "🧹 refactor: A code change that neither fixes a bug nor adds a feature"
     }, {
-        "value": "perf",
+        "value": "🚀 perf",
         "name": "🚀 perf:     A code change that improves performance"
     }, {
-        "value": "test",
+        "value": "🔬‍ test",
         "name": "🔬‍ test:     Adding missing tests"
     }, {
-        "value": "chore",
+        "value": "🛒 chore",
         "name": "🛒 chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation"
     }],
     "scopes": []
